3fe5b0cc7a95ace987c551b993b7d55f3573ec22
Firemark <tiramo@o2.pl>
2013-11-28 10:53:04 +0100

    add result model

---
+++ src/misc.py
@@ -0,0 +1,5 @@
+from math import sqrt
+
+
+def distance(a, b):
+    return sqrt(sum((ea - eb)**2 for ea, eb in zip(a, b)))
--- src/models.py
+++ src/models.py
@@ -1,10 +1,15 @@
 from os.path import basename
+from misc import distance
+from sys import stdout
 
 
 class World(object):
     filename = ""
     k = 0  # maximum cities per route
-    cities = []
+    cities = None
+
+    def __init__(self):
+        self.cities = []
 
     def get_magazine(self):
         return self.cities[0]
@@ -23,3 +28,42 @@ class World(object):
             world.cities = [tuple(int(i) for i in line.split()) for line in f]
 
         return world
+
+
+class Result(object):
+
+    world = None
+    routes = None
+    length = -1.0
+
+    def __init__(self, world):
+        self.world = world
+        self.routes = []
+
+    def add_route(self, route):
+        self.routes.append(route)
+
+    def add_route_with_magazine(self, route):
+        self.add_route([0] + route + [0])
+
+    def compute_length(self):
+        length = 0
+        cities = self.world.cities
+
+        for route in self.routes:
+            prev_city = route[0]
+            for city in route:
+                length += distance(cities[city], cities[prev_city])
+                prev_city = city
+
+        self.length = length
+
+    def print_result(self, stream=stdout):
+        if self.length < 0.0:
+            self.compute_length()
+
+        stream.write("%f\n" % self.length)
+        stream.write("%d\n" % len(self.routes))
+
+        for route in self.routes:
+            stream.write(" ".join(str(i) for i in route) + "\n")
