bc940a2af511c4153227ade31597d851c62e7486
Firemark <tiramo@o2.pl>
2013-12-05 18:06:32 +0100

    crossover method

--- src/models/chromosome.py
+++ src/models/chromosome.py
@@ -1,5 +1,5 @@
-from numpy.random import choice
-from numpy import array_split
+from numpy.random import choice, randint
+from numpy import array_split, in1d
 from utils import compute_distance_with_magazine
 
 
@@ -34,5 +34,19 @@ class Chromosome(object):
     def crossover(self, other):
         """ A x B -> new chromosome """
         new_genes = self.genes.copy()
+        other_genes = other.genes
+        len_genes = len(self.genes)
+
+        a = randint(0, len_genes - 2)
+        b = randint(a, len_genes - 1)
+
+        genes_range = slice(a, b)
+        sliced_other_genes = other_genes[genes_range]
+        sliced_new_genes = new_genes[genes_range]
+
+        mask = in1d(new_genes["index"], sliced_other_genes["index"])
+        new_genes[mask] = sliced_new_genes
+            
+        sliced_new_genes = sliced_other_genes
 
         return Chromosome(new_genes, self.magazine, self.places_in_row)
